5198b12932e8e9662288783047e2287d5cb81d0a
PRAGYA VERMA <102890238+PRAGYA-1998@users.noreply.github.com>
2025-03-23 13:40:26 +0530

    Code files for dino_game_EOG

---
+++ dino_game_eog/control.py
@@ -0,0 +1,36 @@
+# Human Computer Interface (HCI) 
+# Game controller for games like dino and flappy bird
+
+import serial
+import time
+import pyautogui
+from DetectComPort import find_responsive_port
+
+# return time in ms
+def milis():
+    return int(round(time.time() * 1000))
+
+# Arduino serial port interface
+ser = serial.Serial('COM3', 115200, timeout=1)
+
+# Timing variable
+timer = milis()
+
+latency =  24
+                              
+# Infinite loop
+while True:
+    try:
+        # Process serial data
+        data = ser.readline().decode('utf-8').strip()
+        # Debounce
+        if((milis() - timer) > latency):
+            timer = milis()
+            # Virtual spacebar
+            if(int(data)):
+                pyautogui.press('space')  
+                print("jump")
+        ser.flushInput()
+    except Exception :
+        print("\nBlink now...")
+        continue  
